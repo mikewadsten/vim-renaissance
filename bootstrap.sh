baff8a7baae1114acd9e986192a593b94c9a0194
Mike Wadsten <mikewadsten@gmail.com>
2014-02-09 21:09:32 -0600

    Drop leading dots from vim files. Add install script.

---
+++ bootstrap.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+# Variables
+[ -z "$VUNDLE_URI" ] && VUNDLE_URI="https://github.com/gmarik/vundle.git"
+
+# Basic setup tools
+msg() {
+    printf '%b\n' "$1" >&2
+}
+
+success() {
+    if [ "$ret" == "" ]; then
+        ret=$?
+    fi
+    if [ "$ret" -eq 0 ]; then
+        msg "\e[32m[✔]\e[0m ${1}${2}"
+    fi
+}
+
+error() {
+    msg "\e[31m[✘]\e[0m ${1}${2}"
+    exit 1
+}
+
+program_exists() {
+    local ret='0'
+    type $1 >/dev/null 2>&1 || { local ret='1'; }
+
+    # throw error on non-zero return value
+    if [ ! "$ret" -eq '0' ]; then
+        error "$2"
+    fi
+}
+
+# Setup functions
+lnif() {
+    if [ -e "$1" ]; then
+        ln -sf "$1" "$2"
+    fi
+}
+
+verbose_move() {
+    echo "Moving $1 to $2"
+    mv "$1" "$2"
+}
+
+do_backup() {
+    if [ -e "$2" ] || [ -e "$3" ]; then
+        today=`date +%Y%m%d_%s`
+        for i in "$2" "$3"; do
+            [ -e "$i" ] && verbose_move "$i" "$i.$today"
+        done
+        success "$1"
+    fi
+}
+
+setup_vundle() {
+    if [ ! -d "$HOME/.vim/bundle" ]; then
+        mkdir -p "$HOME/.vim/bundle"
+    fi
+
+    if [ ! -e "$HOME/.vim/bundle/vundle" ]; then
+        git clone $VUNDLE_URI "$HOME/.vim/bundle/vundle"
+    fi
+
+    success "$1"
+}
+
+create_symlinks() {
+    endpath="$HOME/vim-renaissance"
+
+    lnif "$endpath/vimrc"           "$HOME/.vimrc"
+    lnif "$endpath/vimrc.bundles"   "$HOME/.vimrc.bundles"
+
+    success "$1"
+}
+
+setup_bundles() {
+    system_shell="$SHELL"
+    export SHELL='/bin/sh'
+    vim -u "$HOME/.vimrc.bundles" +BundleInstall! +BundleClean +qall
+    export SHELL="$system_shell"
+
+    success "$1"
+}
+
+# Main
+program_exists "vim"    "You need to install Vim or add it to your PATH."
+
+do_backup "Your old vim stuff is backed up, to e.g. .vimrc.`date +%Y%m%d%S`" \
+    "$HOME/.vimrc" \
+    "$HOME/.vimrc.bundles"
+
+create_symlinks "Setting up vim symlinks"
+
+setup_vundle    "Vundle is, or should be, set up now."
+
+setup_bundles   "Installed/updated plugins using Vundle"
+
+msg             "\nThanks for installing vim-renaissance"
+msg             "© `date +%Y` Mike Wadsten. https://github.com/mikewadsten"
